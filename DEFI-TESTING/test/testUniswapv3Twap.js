65eae72d32146a706ba46d2f3e2747e93cb84c8e
basu1020 <bvp1020mu@gmail.com>
2022-09-21 11:58:07 +0530

    updated ALphaRouter

--- DEFI-TESTING/test/testUniswapv3Twap.js
+++ DEFI-TESTING/test/testUniswapv3Twap.js
@@ -1,28 +1,28 @@
-// const {ethers} = require('hardhat')
-// const {expect} = require('chai')
+const {ethers} = require('hardhat')
+const {expect} = require('chai')
 
-// const FACTORY = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+const FACTORY = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 
-// //DAI
-// const TOKEN_0 = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-// const DECIMALS_0 = 6n //we are making this a BigInt  
+//DAI
+const TOKEN_0 = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const DECIMALS_0 = 6n //we are making this a BigInt  
 
-// //CHAINLINK
-// const TOKEN_1 = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-// const DECIMALS_1 = 18n
+//CHAINLINK
+const TOKEN_1 = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const DECIMALS_1 = 18n
 
-// //0x5E72d187505367976EE83010bAd667d39AcBA569
+//0x5E72d187505367976EE83010bAd667d39AcBA569
 
-// const FEE = 3000
+const FEE = 3000
 
-// describe('Uniswapv3Twap', () => {
-//     it("get price", async() => {
-//         const UniswapV3Twap = await ethers.getContractFactory("UniswapV3Twap")
-//         const twap = await UniswapV3Twap.deploy(FACTORY, TOKEN_0, TOKEN_1, FEE)
-//         await twap.deployed()
-//         const price = await twap.estimateAmountOut(TOKEN_1, 10n ** DECIMALS_1, 10)
-//         console.log(price)
-//     })
-// })
+describe('Uniswapv3Twap', () => {
+    it("get price", async() => {
+        const UniswapV3Twap = await ethers.getContractFactory("UniswapV3Twap")
+        const twap = await UniswapV3Twap.deploy(FACTORY, TOKEN_0, TOKEN_1, FEE)
+        await twap.deployed()
+        const price = await twap.estimateAmountOut(TOKEN_1, 10n ** DECIMALS_1, 10)
+        console.log(price)
+    })
+})
 
 
--- Uniswap-V3-SDK/AlphaRouter.js
+++ Uniswap-V3-SDK/AlphaRouter.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const { AlphaRouter } = require('@uniswap/smart-order-router')
-const {Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core')
-const { ethers, BigNumber } = require('ethers')
+const {Token,
+    CurrencyAmount,
+    TradeType,
+    Percent 
+} = require('@uniswap/sdk-core')
+const { ethers, 
+    BigNumber 
+} = require('ethers')
 const JSBI = require('jsbi')
 const ERC20ABI = require('./abi.json')
+
 const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
 
 const WALLET_ADDRESS = process.env.WALLET_ADDRESS
@@ -13,17 +20,18 @@ const ALCHEMY_TEST_URL = process.env.ALCHEMY_TEST_URL
 const web3Provider = new ethers.providers.JsonRpcProvider(ALCHEMY_TEST_URL)
 const chainId = 5
 
+//initiating router
 const router = new AlphaRouter({
     chainId: chainId,
     provider: web3Provider
 })
 
-const name0 = 'Wrapped Ether'
+const name0 = 'WETH Token'
 const symbol0 = 'WETH'
 const decimals0 = 18
 const address0 = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'
 
-const name1 = 'Uniswap Token'
+const name1 = 'Uniswap token'
 const symbol1 = 'UNI'
 const decimals1 = 18
 const address1 = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
@@ -31,13 +39,13 @@ const address1 = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'
 const WETH = new Token(chainId, address0, decimals0, symbol0, name0)
 const UNI = new Token(chainId, address1, decimals1, symbol1, name1)
 
-const wei = ethers.utils.parseUnits('0.01', 18)
+const wei = ethers.utils.parseUnits('0.001', 18)
 const inputAmmount = CurrencyAmount.fromRawAmount(WETH, JSBI.BigInt(wei))
 
 async function main () {
 
-    // intitate route 
-    const route = await router.route(
+    // Calling route : The route method returns all the swap calldata and gas information needed for submitting an optimal swap to the chain.
+    const route = await router.route (
         inputAmmount,
         UNI,
         TradeType.EXACT_INPUT,
@@ -48,30 +56,37 @@ async function main () {
         }
     )
 
-    // define transaction
+    // displaying some information dervied from route. 
+    console.log("Gas",BigNumber.from(route.gasPriceWei)) // gasInfo
+    console.log("Trade Pairs", route.trade) // Trading pair
+    console.log("Amount of Sent Token", route.quote)
+    console.log("Route", route.route)
+
+    // preparing transaction object
     const transaction = {
         data: route.methodParameters.calldata,
         to: V3_SWAP_ROUTER_ADDRESS,
         value: BigNumber.from(route.methodParameters.value),
         from: WALLET_ADDRESS,
-        gasPrice: BigNumber.from('100000000000'),
+        gasPrice: BigNumber.from(route.gasPriceWei),
         gasLimit: ethers.utils.hexlify(1000000)
     } 
 
-    // connect wallet
+    // connecting wallet
     const wallet = new ethers.Wallet(WALLET_SECRET)
     const connectedWallet = wallet.connect(web3Provider)
 
-    // approving the V3 router for it. 
-    const approvalAmount = ethers.utils.parseUnits('1', 18).toString()
+    // approving the V3 router for WETH token. 
+    const approvalAmount = ethers.utils.parseUnits('0.001', 18).toString()
     const contract0 = new ethers.Contract(address0, ERC20ABI, web3Provider)
     await contract0.connect(connectedWallet).approve(
         V3_SWAP_ROUTER_ADDRESS,
         approvalAmount
     )
 
+    //sending the Transaction
     await connectedWallet.sendTransaction(transaction)
     // tradeTransaction.wait()
-}
+} 
 
 main()
