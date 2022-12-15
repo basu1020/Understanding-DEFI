6e5b4542431517a73b39c7d49674536e5fe70699
basu1020 <bvp1020mu@gmail.com>
2022-12-15 23:51:19 +0530

    completed SupplyAndRedeem and added BorowAndRedeem

---
+++ COMPOUND/contracts/BorrowAndRedeem.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPl-3.0
+
+pragma solidity ^0.8.10;
+
+import {CErc20} from "../compound-protocol/compound-protocol/contracts/CErc20.sol";
+import {ERC20} from "../compound-comet/comet/contracts/ERC20.sol";
+
+contract BorrowAndRepay {
+    
+}
--- COMPOUND/contracts/SupplyAndRedeem.sol
+++ COMPOUND/contracts/SupplyAndRedeem.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPl-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import {Comet} from "../compound-comet/comet/contracts/Comet.sol";
 import {CometFactory} from "../compound-comet/comet/contracts/CometFactory.sol";
@@ -9,10 +9,25 @@ import {CErc20} from "../compound-protocol/compound-protocol/contracts/CErc20.so
 import{ERC20} from "../compound-comet/comet/contracts/ERC20.sol";
 
 contract CompoundSupplyAndRedeem is CometConfiguration {
-    CErc20 public cToken = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F; //USDC
-    ERC20 public 
+    CErc20 public cToken = CErc20(0x3EE77595A8459e93C2888b13aDB354017B198188); //cUSDC
+    ERC20 public token = ERC20(0x07865c6E87B9F70255377e024ace6630C1Eaa37F); //USDC
 
-    function supply(uint amount) public {
-        
+    function supply(uint amount) external {
+        token.transferFrom(msg.sender, address(this), amount);
+        token.approve(address(cToken), amount);
+        require(cToken.mint(amount) == 0, "Not today Son");
+        cToken.transfer(msg.sender, amount);
+    }
+
+    function getBalanceOfCtoken() public returns(uint256){
+        return cToken.balanceOf(msg.sender);
+    }
+
+    function redeem(uint amount) external {
+        cToken.transferFrom(msg.sender, address(this), amount);
+        uint256 beforeRedeem = token.balanceOf(address(this));
+        require(cToken.redeem(amount) == 0, "sh*t");
+        uint256 afterRedeem = token.balanceOf(address(this));
+        token.transfer(msg.sender, afterRedeem - beforeRedeem);
     }
 }
